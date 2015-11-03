1e72c215e75ae648f9258d54b1eb1e2ae3a3091e
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-11-03 14:44:16 +0530

    Minor encoding changes

--- fun_commitstrip-downloader.py
+++ fun_commitstrip-downloader.py
@@ -1,23 +1,28 @@
 import urllib2
 from pyquery import PyQuery as pq
 from lxml import etree
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def save_pic(url, count):
 	photo = urllib2.urlopen(url)
 	photo = photo.read()
-	fp = open(count+".jpg", "w")
+	fp = open(str(count)+".jpg", "w")
 	fp.write(photo)
 	fp.close()
 
 FIRST_URL = "http://www.commitstrip.com/en/2012/02/22/interview/"
 
+
+print "\nLoading commitstrip.com...\n"
 d = pq(url=FIRST_URL)
 count=1
 
 while 1:
-	print "Saving comic number "+count+"...\n"
-	count += 1
+	print "Saving comic no. "+str(count)+""
 	NEXT_URL = d('.nav-next a').attr('href')
-	save_pic(d('img.alignnone').attr('src'))
+	save_pic(d('img.alignnone').attr('src'), count)
 	d = pq(url=NEXT_URL)
-
+	count += 1
